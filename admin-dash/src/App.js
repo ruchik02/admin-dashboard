f70bc018b60bc766133c7c3b6839c104e92336c7
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2024-01-10 23:44:50 +0530

    add themes

--- admin-dash/src/App.js
+++ admin-dash/src/App.js
@@ -1,5 +1,20 @@
+import Topbar from "./screens/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
