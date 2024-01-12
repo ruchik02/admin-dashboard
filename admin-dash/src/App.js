1bc7f015d113f96677616e5be1320fdbdd4d403f
Ruchika Sharma <ruchikasharma@Ruchikas-MacBook-Pro.local>
2024-01-13 00:45:42 +0530

    route setup

--- admin-dash/src/App.js
+++ admin-dash/src/App.js
@@ -1,15 +1,41 @@
 import Topbar from "./screens/global/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./screens/dashboard";
+import Contacts from "./screens/contacts";
+import Bar from "./screens/bar";
+import Team from "./screens/team";
+import Calendar from "./screens/calendar";
+import Faq from "./screens/faq";
+import Geography from "./screens/geography";
+import Pie from "./screens/pie";
+import Line from "./screens/line";
+import Invoice from "./screens/invoices";
+import Form from "./screens/form";
+
 function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-        <CssBaseline/>
+        <CssBaseline />
         <div className="app">
           <main className="content">
-            <Topbar/>
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/invoices" element={<Invoice />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<Faq />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/geo" element={<Geography />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/pie" element={<Pie />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
---
+++ admin-dash/src/screens/geography/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Geography = () => {
+  return (
+    <div>
+      Geography
+    </div>
+  )
+}
+
+export default Geography
