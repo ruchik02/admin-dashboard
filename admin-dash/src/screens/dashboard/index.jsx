c00764a6833127fa784b6d43f4c1492e5cb541fa
ruchika sharma <ruchikasharma.cse19@chitkarauniversity.edu.in>
2024-01-14 20:51:29 +0530

    sidebar added , header component added reinstall sidebar package downgrade package

--- admin-dash/src/screens/dashboard/index.jsx
+++ admin-dash/src/screens/dashboard/index.jsx
@@ -1,11 +1,18 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Headers from '../../components/Headers'
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+   <Box m="20px">
+    {/* Header */}
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Headers title="Dashboard" subtitle="Welcome to your dashboard"/>
+
+    </Box>
+    
+   </Box>
   )
 }
 
-export default Dashboard
+export default Dashboard;
